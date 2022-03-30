2b64bcc52b731bb1669b58b316111357d8c77e0c
Gustavo <100697370+gustavoOD@users.noreply.github.com>
2022-03-30 09:30:11 -0500

    commit

---
+++ STA/app.js
@@ -0,0 +1,16 @@
+/*Aplicacion que me guarde datos en una table*/
+var nombre;
+var apellido;
+var telefono;
+var email;
+var direccion;
+var fecha;
+var nota;
+var tabla = document.getElementById("table");
+var boton = document.getElementById("guardar");
+var contador = 1;
+/*console.log("Java Scrip Funcionando")*/
+
+guardar.addEventListener("click",function(){
+    console.log("Ha dado click");
+})
